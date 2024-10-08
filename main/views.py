809848361d145c4ec956a1a54bad4851f95fe2bc
daf <daffa199naufal@gmail.com>
2024-10-08 12:09:20 +0700

    tugas 6 done

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -8,19 +10,20 @@ from django.core import serializers
 from django.shortcuts import render, redirect, reverse
 from main.forms import ProductForm
 from main.models import Product
+from django.utils.html import strip_tags
 import datetime
 
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
-    product_entries = Product.objects.filter(user=request.user)
+    # product_entries = Product.objects.filter(user=request.user)
     context = {
         'name' : request.user.username,
         'npm': '2306213003', 
         'class': 'PBP D',
         'description': 'this is capybara',
         'chill_level': '10',
-        'product_entries' : product_entries,
+        # 'product_entries' : product_entries,
         'last_login': request.COOKIES['last_login']
     }
 
@@ -65,6 +68,25 @@ def delete_product(request, id):
     return HttpResponseRedirect(reverse('main:show_main'))
 
 
+@csrf_exempt
+@require_POST
+def add_product_entry_ajax(request):
+    name = strip_tags(request.POST.get("name"))
+    price = request.POST.get("price")
+    description = strip_tags(request.POST.get("description"))
+    chill_level = request.POST.get("chill_level")
+    user = request.user
+
+    new_product = Product(
+        name=name, price=price,
+        description=description, chill_level = chill_level,
+        user=user
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
+
+
 def register(request):
     form = UserCreationForm()
 
@@ -89,6 +111,8 @@ def login_user(request):
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
+      else:
+          messages.error(request, "Invalid username or password. Please try again.")
 
    else:
       form = AuthenticationForm(request)
@@ -104,11 +128,11 @@ def logout_user(request):
 
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
